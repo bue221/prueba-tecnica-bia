59c8856af21f28b6b71c9fa7359f52f4498a90dc
Andres Plaza (@bue221) <camiloplaza3@gmail.com>
2025-05-12 09:46:57 -0500

    fix: ensure testPathIgnorePatterns includes '/tests/' for proper test exclusion

--- jest.config.ts
+++ jest.config.ts
@@ -17,6 +17,7 @@ const config: Config = {
   coverageProvider: "v8",
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/tests/'],
 };
 
 export default createJestConfig(config)
